35200ad8ef8420a04210974078b4a4360571bb31
johan.martinsson <johan.martinsson@dexyos.fr>
2022-03-18 09:37:23 +0100

    extract serveMenu

---
+++ java/src/main/java/inheritance/Waiter.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class Waiter {
+    public static String serveMenu(BestValueMenu bestValueMenu) {
+        return bestValueMenu.starter() + "\n" +
+                bestValueMenu.mainCourse() + "\n" +
+                bestValueMenu.dessert();
+    }
+}
--- java/src/test/java/kata/MenuTest.java
+++ java/src/test/java/kata/MenuTest.java
@@ -3,6 +3,7 @@ package kata;
 import inheritance.BestValueMenu;
 import inheritance.GourmetsMenu;
 import inheritance.MeatLoversMenu;
+import inheritance.Waiter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +14,7 @@ public class MenuTest {
     @Test
     void bestValueMenu() throws Exception {
         BestValueMenu bestValueMenu = new BestValueMenu();
-        String result = serveMenu(bestValueMenu);
+        String result = Waiter.serveMenu(bestValueMenu);
         Assertions.assertEquals("sallad\n" +
                 "quiche lorraine\n" +
                 "brownie", result);
@@ -23,7 +24,7 @@ public class MenuTest {
     @Test
     void GourmetsMenu() throws Exception {
         GourmetsMenu gourmetsMenu = new GourmetsMenu();
-        String result = serveMenu(gourmetsMenu);
+        String result = Waiter.serveMenu(gourmetsMenu);
         Assertions.assertEquals("sallad\n" +
                 "quiche lorraine\n" +
                 "crème caramel", result);
@@ -32,16 +33,11 @@ public class MenuTest {
     @Test
     void meatLoversMenu() throws Exception {
         MeatLoversMenu meatLoversMenu = new MeatLoversMenu();
-        String result = serveMenu(meatLoversMenu);
+        String result = Waiter.serveMenu(meatLoversMenu);
         Assertions.assertEquals("sallad\n" +
                 "filet mignon\n" +
                 "brownie", result);
 
     }
 
-    private String serveMenu(BestValueMenu bestValueMenu) {
-        return bestValueMenu.starter() + "\n" +
-                bestValueMenu.mainCourse() +  "\n" +
-                bestValueMenu.dessert();
-    }
 }
